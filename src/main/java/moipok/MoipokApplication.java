53834f3aad35d0f8b3c578f10d47abcd14c849bd
Frodo Barbera <fbarbera@mi-f5.msk.21-school.ru>
2021-06-15 21:35:41 +0300

    start

---
+++ src/main/java/moipok/MoipokApplication.java
@@ -0,0 +1,13 @@
+package moipok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoipokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoipokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/moipok/MoipokApplicationTests.java
@@ -0,0 +1,13 @@
+package moipok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoipokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
